ed6936aa5ddf5631da938b5473e0109ea1214b90
Anna Grabowska <aniagoodness@gmail.com>
2020-06-29 20:38:18 +0200

    js2

--- src/js/script.js
+++ src/js/script.js
@@ -1,11 +1,4 @@
-'use strict';
-
-function toggleMenu (visible) {
-  document.querySelector('.menu').classList.toggle('show', visible);
-}
-toggleMenu();
-
 document.querySelector('.hamburger').addEventListener('click', function (e) {
   e.preventDefault();
-  toggleMenu();
+  document.querySelector('.sidebar').classList.toggle('hide');
 });
